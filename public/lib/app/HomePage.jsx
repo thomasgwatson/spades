40d1edb7d02daefceac4452c5aefd5eef1d0d118
thomasgwatson <thomasgabriel.watson@gmail.com>
2015-09-03 13:53:32 -0700

    Revert failed chrome-mobile fixes

--- public/lib/app/HomePage.jsx
+++ public/lib/app/HomePage.jsx
@@ -21,13 +21,11 @@ var HomePage = React.createClass({
       me.scrollToCenter()
     });
   },
-  componentWillReceiveProps: function(){
-    this.scrollToCenter()
-  },
   scrollToCenter: function(){
     var imageWidth = document.getElementById('img').offsetWidth
     var offSetRight = (imageWidth - document.documentElement.offsetWidth )/2
-    this.setState({offSet: offSetRight, bottom: 10, imageWidth: imageWidth, imageLoaded: true})
+    window.scrollTo(offSetRight, 0);
+    this.setState({offset: offSetRight, bottom: 10, imageWidth: imageWidth, imageLoaded: true})
   },
   rollDownToMap: function(evt){
     evt.preventDefault()
@@ -37,13 +35,8 @@ var HomePage = React.createClass({
         }, 1000);
   },
   render: function(){
-    var screeningPageStylings = {height: (this.state.imageLoaded ? "0%" : "100%"), width: (this.state.imageLoaded ? "100%" : "600%"), zIndex: 400, top: 0, bottom: 0, right: (-this.state.offset - 1000), left: this.state.offset, position: "absolute" }
-    var screeningTextStylings = { textAlign: 'center', height: "100%", width: (this.state.imageLoaded ? "100%" : "150%"), zIndex: 401, top: 0, bottom: 0, position: 'absolute', color: "rgb(15,15,15)", fontFamily: "indie-font", fontSize: 50, right: -this.state.offset, left: this.state.offset}
-
-    if(this.state.offSet){
-      $(window).scrollTo({left: this.state.offSet, top:0});
-    }
-
+   var screeningPageStylings = {height: (this.state.imageLoaded ? "0%" : "100%"), width: (this.state.imageLoaded ? "100%" : "600%"), zIndex: 400, top: 0, bottom: 0, right: (-this.state.offset - 1000), left: this.state.offset, position: "absolute" }
+   var screeningTextStylings = { textAlign: 'center', height: "100%", width: (this.state.imageLoaded ? "100%" : "150%"), zIndex: 401, top: 0, bottom: 0, position: 'absolute', color: "rgb(15,15,15)", fontFamily: "indie-font", fontSize: 50, right: -this.state.offset, left: this.state.offset}
     return(
       <div className="container heighty">
         <div style={screeningPageStylings} className="background-color-woo click-thru">
@@ -53,10 +46,10 @@ var HomePage = React.createClass({
         <img ref='img' id='img' src={'/images/backing/' + this.state.image} className="bg" />
         <div className='scrim' id='scrim' style={{width: this.state.imageWidth}}>
         </div>
-        <div className='title-text' id='title-text' style={{left: this.state.offSet + 25 + 'px', bottom: 10}}>
+        <div className='title-text' id='title-text' style={{left: this.state.offset + 25 + 'px', bottom: 10}}>
           <span>Graffi.so</span>
         </div>
-        <div className='blurb-text' id='blurb-text' style={{right: -this.state.offSet + 25 + 'px', bottom: this.state.bottom}}>
+        <div className='blurb-text' id='blurb-text' style={{right: -this.state.offset + 25 + 'px', bottom: this.state.bottom}}>
           <span>Street Art Discovery Engine</span>{" "}<span onClick={this.rollDownToMap} className="fa fa-chevron-circle-down" id="arrow"></span>
         </div>
       </div>
